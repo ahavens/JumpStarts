77f803d1fa3e9fae97420ac3f779a7667db4fdce
gavinpham <gpham@smu.edu>
2017-04-19 00:02:45 -0500

    more developments

--- web/assets/js/getCalendar.js
+++ web/assets/js/getCalendar.js
@@ -2,23 +2,33 @@ $(document).ready(function() {
     $.ajax({
         type: "GET",
         //should go to this URL
-        url: 'http://54.201.62.39/php/user.php',
+        url: 'http://54.201.62.39/php/videos.php',
         data:"{}",
         success: function(data)
         {
-            if (data === 'got user') {
+            if (data === '?????????????????????????????????') {
 
-                /* 
-                ** These lines replace data fields with that of the user's data from the db
-                ** Self Memo: Not sure if this is functional, idk how to use the cookies with this. (Gavin)
-                */
+              /* 
+              ** These lines replace data fields with that of the user's data from the db
+              ** Self Memo: Not sure if this is functional, idk how to use the cookies with this. (Gavin)
+              ** Also: this query confuses me. Am I just getting the videos in total, with like a total viewcount? idk.
+              */
 
-                //Populate Manifesto
-               $('#fname_1').text(data.fname);
-               $('#project_name').text(data.project_name);
-               $('#fname_2').text(data.fname);
-               $('#time').text((data.time).toString());
-               $('#day').text(data.day);
+              //Populate Calendar
+              $('#calendar_0').text(data.calendar[0]);
+              $('#calendar_1').text(data.calendar[1]);
+              $('#calendar_2').text(data.calendar[2]);
+              $('#calendar_3').text(data.calendar[3]);
+              $('#calendar_4').text(data.calendar[4]);
+              $('#calendar_5').text(data.calendar[5]);
+
+              //Populate Badges
+              var total_views = 0;
+              data = $.parseJSON(data.calendar);
+              $.each(data, function(i, item) {
+                alert(item);
+              });
+            
             }
             else {
                 alert(data);
